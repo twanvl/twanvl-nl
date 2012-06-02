430a28d5f8f2261b2d92f605497fbe788ce1debc
Twan van Laarhoven <twanvl@gmail.com>
2012-06-02 14:36:11 +0200

    more spam filtering

--- lib/Comment.php
+++ lib/Comment.php
@@ -4,6 +4,19 @@
 // Comments
 // -----------------------------------------------------------------------------
 
+function detectUTF8($string){
+	// from http://nl.php.net/manual/en/function.iconv.php
+	return preg_match('%(?:
+	[\xC2-\xDF][\x80-\xBF]             # non-overlong 2-byte
+	|\xE0[\xA0-\xBF][\x80-\xBF]        # excluding overlongs
+	|[\xE1-\xEC\xEE\xEF][\x80-\xBF]{2} # straight 3-byte
+	|\xED[\x80-\x9F][\x80-\xBF]        # excluding surrogates
+	|\xF0[\x90-\xBF][\x80-\xBF]{2}     # planes 1-3
+	|[\xF1-\xF3][\x80-\xBF]{3}         # planes 4-15
+	|\xF4[\x80-\x8F][\x80-\xBF]{2}     # plane 16
+	)+%xs', $string);
+}
+
 class Comment {
 	public $id = 0;
 	public $date;
@@ -23,11 +36,11 @@ class Comment {
 	// simple spam filter
 	function is_spam() {
 	    // URL blacklist
-		if (preg_match('@vibramfivefinger.org|northfaceoutlet.com|jacketsnorthface.com|uggbootsole.com|buyuggbootsuk.com|cheapuggs-uk.com|jerseysusa.com|capssupplier.com|cheapchristianshoes.com|.cheapghdivstyler.com|webstore.com|hairstraighteners.com|napervilleillocksmith.com|medicadeals.com|officialtiffanybracelet.com|thecylinder.net|onlybrandshoes.com|jordanshops.com|trade-shops.com|coach-us-outlet.com|cellphone-case.net|mes-baskets.com|edhardystyle.net|sportshoeonsale.com|admissionsessay.net|levitrapharmacies.com|www.wholesale|shox...com|ds-b.jp|monclerukshops.com|boots-top.com|nike-discount[.]com|onsale[.]com|pronosearch.com|disocuntedhardy.com|nike-gold.com|shop.info|boots-gold.com|tablet-pc.info|monsterstereoheadset.com|monclertopstores.com|topstores.com|topsalesmbt.com|bride-shops.com|monsterbeatsselling.com|storesale.com|relatiefront.nl|topbag2u.com|frmonclerjacket.com|uggs(sale|clearance)(online)?[.](net|com|org)|starjordan.com|coatsmoncler.com|own-jordan.com|onlineshoes[a-z]*.com|diorbag4u.com|ebayhermeskelly.com|onlinemonsterbeatssale.com|findremedyfast.com|mlbjerseys.com|airjordanfrance.net|jerseysdiscount.net@i',$this->body . ' ' . $this->author_url)) return true;
+		if (preg_match('@vibramfivefinger.org|northfaceoutlet.com|jacketsnorthface.com|uggbootsole.com|buyuggbootsuk.com|cheapuggs-uk.com|jerseysusa.com|capssupplier.com|cheapchristianshoes.com|.cheapghdivstyler.com|webstore.com|hairstraighteners.com|napervilleillocksmith.com|medicadeals.com|officialtiffanybracelet.com|thecylinder.net|onlybrandshoes.com|jordanshops.com|trade-shops.com|coach-us-outlet.com|cellphone-case.net|mes-baskets.com|edhardystyle.net|sportshoeonsale.com|admissionsessay.net|levitrapharmacies.com|www.wholesale|shox...com|ds-b.jp|monclerukshops.com|boots-top.com|nike-discount[.]com|onsale[.]com|pronosearch.com|disocuntedhardy.com|nike-gold.com|shop.info|boots-gold.com|tablet-pc.info|monsterstereoheadset.com|monclertopstores.com|topstores.com|topsalesmbt.com|bride-shops.com|monsterbeatsselling.com|storesale.com|relatiefront.nl|topbag2u.com|frmonclerjacket.com|uggs(sale|clearance)(online)?[.](net|com|org)|starjordan.com|coatsmoncler.com|own-jordan.com|onlineshoes[a-z]*.com|diorbag4u.com|ebayhermeskelly.com|onlinemonsterbeatssale.com|findremedyfast.com|mlbjerseys.com|airjordanfrance.net|jerseysdiscount.net|norxonline.com|order.ed-express.info|cheappillsed.us|edgroup.be/vaigra|edpillsrx.us|remroom.ru|(snapbackhats|airmax|oakleysunglass)(bar|outlet|zone).com|isuprashoes.com|servcss.com|playgoogle.ws@i',$this->body . ' ' . $this->author_url . ' ' . $this->author_email)) return true;
 	    // URL blacklist
 		if (preg_match('@[.](com|net|org)/[-_a-z0-9/.]*\b(air-jordan)\b@i',$this->body . ' ' . $this->author_url)) return true;
 		// Word blacklist
-		if (preg_match('@\b(free online dating|cialis|sex chat|sex dating|purchase adderall|marlboro cigarettes|viagra|car insurance|auto insurance|generic levitra|generic ultram|internet pharmacy|online pharmacy|Tiffany Bracelets|Nike shoes|cheap shox|Ugg boots|cheap nike|Louis Vuitton Handbags|Drug Rehabilitation|casinos?|xanax|klonopin|cheap\s\S*\spills|adderall|buy cheap generic|buy generic online|aciphex buy online|prednisone online|porno videos|Fur-Collar Puffer Jacket|100% wool collar|Nike.*Addidas.*Reebok|MBT Womans Tunisha|do not iron the sportswear|cheap mbt shoes|Wedding Bride Gown|Limited Edition Sale|"uggs clearance"|Cheap Jordan Shoes|cheap uggs?|(Versace|Carolina Herrera) (?:\s*(Hobo|Khaki|Beige|Black|Gold)\s*)*(Handbag|Bag|Bags))\b@i',$this->body . ' ' . $this->author_name)) return true;
+		if (preg_match('@\b(free online dating|cialis|sex chat|sex dating|purchase adderall|marlboro cigarettes|viagra|car insurance|auto insurance|generic levitra|generic ultram|internet pharmacy|online pharmacy|Tiffany Bracelets|Nike shoes|cheap shox|cheap in india|sex with your partner|Ugg boots|cheap nike|Louis Vuitton Handbags|Drug Rehabilitation|casinos?|xanax|klonopin|cheap\s\S*\spills|adderall|buy cheap generic|buy generic online|aciphex buy online|prednisone online|porno videos|Fur-Collar Puffer Jacket|100% wool collar|Nike.*Addidas.*Reebok|MBT Womans Tunisha|do not iron the sportswear|cheap mbt shoes|Wedding Bride Gown|Limited Edition Sale|"uggs clearance"|Cheap Jordan Shoes|cheap uggs?|(Versace|Carolina Herrera) (?:\s*(Hobo|Khaki|Beige|Black|Gold)\s*)*(Handbag|Bag|Bags)|trouble getting an erection)\b@i',$this->body . ' ' . $this->author_url . ' ' . $this->author_name)) return true;
 		// Heuristic : link count
 		$noa = preg_replace('@[<]a[ ].*?[<]/a[^a-z]|http:[-a-zA-Z0-9_/.]+@i','',$this->body);
 		$noa = strip_tags($noa);
@@ -48,6 +61,11 @@ class Comment {
 			// url == name
 			if (preg_match("@^[A-Za-z0-9]+\s+<a href=\"http://([A-Za-z0-9]+)[.]com[/]\">\\1</a>$@",$this->body)) return true;
 		}
+		// weirdness in author name
+		if (preg_match('@<a href=|\[url[=\]]@i',$this->author_name . ' ' . $this->author_url . ' ' . $this->author_email)) return true;
+		// Heuristic: valid UTF8
+		//if (!mb_detect_encoding($this->body . ' ' . $this->author_name, 'UTF-8', true)) return true;
+		//if (strlen($this->body) > iconv_strlen($this->body, 'UTF-8//TRANSLIT')) return true;
 		// Otherwise not spam
 		return false;
 	}
