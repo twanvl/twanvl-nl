4565949860257b115fad0f4f719472cfe8107e41
Twan van Laarhoven <twanvl@gmail.com>
2013-06-22 19:02:55 +0200

    some highlighting improvements for agda.

--- lib/AgdaFormat.php
+++ lib/AgdaFormat.php
@@ -17,11 +17,12 @@ require_once('lib/HaskellFormat.php');
 global $haskell_lang;
 global $agda_lang;
 $agda_lang = $haskell_lang;
-$agda_lang['keyglyph'] = substr($agda_lang['keyglyph'],0,-1) . '|[∀→_]@u';
-$agda_lang['keyword'] = '@\b(if|then|else|module|import|qualified|hiding|where|let|in|case|of|newtype|default|infix|infixr|infixl|(?:data|type)(?: family)?|class|instance|forall|exists|deriving|do|record|field|open|public|private|renaming|__keyword__[[:alnum:]]+)(?=$|[ (){}])@';
+$agda_lang['keyglyph'] = '@=>|=|->|::|\\\\|<-|\||[∀→_]@u';
+$agda_lang['keyword'] = '@\b(if|then|else|module|open|import|qualified|hiding|where|let|in|case|of|newtype|default|infix|infixr|infixl|data|class|instance|forall|exists|deriving|do|record|field|open|public|private|renaming|postulate|__keyword__[[:alnum:]]+)(?=$|[ (){}])@';
 $agda_lang['conop'] = "@[⊎ℕ]@u";
-$agda_lang['varop'] = "@([-:\\@#$%^*.|=+<>&~/\\\\\'⟶≈⟨⟩⌈⌉⌊⌋≤◂≡≢?∘]|!!?(?!!))+[^ (){}_]*(__[{][^}]*[}])?@u";
+$agda_lang['varop'] = "@([-:\\@#$%^*.|=+<>&~/\\\\\'⟶≈⟨⟩⌈⌉⌊⌋≤◂≡≢?∘\\[\\]]|!!?(?!!))+[^ (){}_]*(__[{][^}]*[}])?@u";
 $agda_lang['conid'] = "@\\b(?:[[:upper:]][[:alnum:]']*[.])*[[:upper:]][^ (){}_.]*(?=$|[ (){}])@u";
 $agda_lang['varid'] = "@\\b(?:[[:upper:]][[:alnum:]']*[.])*[[:lower:]][^ (){}_.]*(?=$|[ (){}])@u";
 $agda_lang['hole'] = "@[{]!(.*?)![}]@u";
+unset($agda_lang['listcon']);
 
