24f51b89ced99d3bedfd03963dd9b554bf1f35d8
Twan van Laarhoven <twanvl@gmail.com>
2013-06-22 21:18:07 +0200

    'with' keyword

--- lib/AgdaFormat.php
+++ lib/AgdaFormat.php
@@ -18,7 +18,7 @@ global $haskell_lang;
 global $agda_lang;
 $agda_lang = $haskell_lang;
 $agda_lang['keyglyph'] = '@=>|=|->|::|\\\\|<-|\||[∀→_]@u';
-$agda_lang['keyword'] = '@\b(if|then|else|module|open|import|qualified|hiding|where|let|in|case|of|newtype|default|infix|infixr|infixl|data|class|instance|forall|exists|deriving|do|record|field|open|public|private|renaming|postulate|__keyword__[[:alnum:]]+)(?=$|[ (){}])@';
+$agda_lang['keyword'] = '@\b(if|then|else|module|open|import|qualified|hiding|where|let|in|case|of|newtype|default|infix|infixr|infixl|data|class|instance|forall|exists|deriving|do|record|field|open|public|private|renaming|postulate|with|__keyword__[[:alnum:]]+)(?=$|[ (){}])@';
 $agda_lang['conop'] = "@[⊎ℕ]@u";
 $agda_lang['varop'] = "@([-:\\@#$%^*.|=+<>&~/\\\\\'⟶≈⟨⟩⌈⌉⌊⌋≤◂≡≢?∘\\[\\]]|!!?(?!!))+[^ (){}_]*(__[{][^}]*[}])?@u";
 $agda_lang['conid'] = "@\\b(?:[[:upper:]][[:alnum:]']*[.])*[[:upper:]][^ (){}_.]*(?=$|[ (){}])@u";
