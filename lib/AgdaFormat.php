35bf64018936aec4287ffcf545266a55e6cf757d
Twan van Laarhoven <twanvl@gmail.com>
2017-03-24 00:28:00 +0100

    Added a 'small space' code style for agda

--- lib/AgdaFormat.php
+++ lib/AgdaFormat.php
@@ -26,16 +26,16 @@ $agda_lang['pragma'] = $haskell_lang['pragma'];
 $agda_lang['comment'] = $haskell_lang['comment'];
 $agda_lang['str'] = $haskell_lang['str'];
 $agda_lang['chr'] = $haskell_lang['chr'];
-$agda_lang['num'] = $haskell_lang['num'];
+$agda_lang['num'] = '@-?\b[0-9]+(?=$|[ (){};_])@u';
 $agda_lang['keyglyph'] = '@=>|=|->|:|\\\\|<-|\||[∀→_]@u';
 $agda_lang['keyword'] = '@\b(if|then|else|module|open|import|qualified|hiding|where|let|in|case|of|newtype|default|infix|infixr|infixl|data|class|instance|forall|exists|deriving|do|record|field|open|public|private|renaming|postulate|with|__keyword__[[:alnum:]]+)(?=$|[ (){}])@u';
 $agda_lang['agda-fun']  = "@\b(cong|subst|Set|Eq|Σ|Π|Iso)(?=$|[ (){}])@u";
 $agda_lang['agda-ctor'] = "@\b(refl|,|\[\]|0|zero|suc|here|there|acc)(?=$|[ (){}])@u";
 $agda_lang['agda-proj'] = "@\b(proj₁|proj₂)(?=$|[ (){}])@u";
+$agda_lang['small-space'] = '@(?<=\[)[ ]|[ ](?=\])@u';
 $agda_lang['conop'] = "@[⊎ℕ]@u";
 $agda_lang['varop'] = "@([-:\\@#$%^*.|=+<>&~/\\\\\'⟶≈⟨⟩⌈⌉⌊⌋≤◂≡×≢?∘\\[\\]]|!!?(?!!))+[^ (){}_]*(__[{][^}]*[}])?@u";
 $agda_lang['conid'] = "@\\b(?:[[:upper:]][[:alnum:]']*[.])*[[:upper:]][^ (){}_.;]+(?=$|[ (){};_])@u";
 $agda_lang['varid'] = "@\\b(?:[[:upper:]][[:alnum:]']*[.])*[[:lower:]][^ (){}_.;^]*(?=$|[ (){};_])@u";
 $agda_lang['hole'] = "@[{]!(.*?)![}]@u";
-unset($agda_lang['listcon']);
 
