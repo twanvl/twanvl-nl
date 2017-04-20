3772a3ba67c3034bfc34c7181b14312a3e447c49
Twan van Laarhoven <twanvl@gmail.com>
2017-04-20 23:17:41 +0200

    more keywords, added "-- HIDE" to hide parts of types

--- lib/AgdaFormat.php
+++ lib/AgdaFormat.php
@@ -6,8 +6,16 @@
 
 class AgdaFormat extends HaskellFormat {
   public function __construct() {
-	global $agda_lang;
+    global $agda_lang;
     parent::__construct($agda_lang);
+  }
+  public function format($code) {
+    if (preg_match('@--\s*HIDE (.*)|{-\s*HIDE (.*)\s*-}@', $code, $ma)) {
+      $to_hide = $ma[1];
+      $code = preg_replace('@\s*--\s*HIDE (.*)|{-\s*HIDE (.*)\s*-}@', '', $code);
+      $code = preg_replace("@[{](($to_hide)\s*)+[}]\s*|\\s+\\b($to_hide)\\b|\\b($to_hide)\\b\\s*\\s*@u", '', $code);
+    }
+    return parent::format($code);
   }
 }
 
@@ -27,15 +35,15 @@ $agda_lang['comment'] = "@---*(?![-:\\@#$%^*.|=+<>&~]).*|{-.*-}@u";
 $agda_lang['str'] = $haskell_lang['str'];
 $agda_lang['chr'] = $haskell_lang['chr'];
 $agda_lang['num'] = '@-?\b[0-9]+(?=$|[ (){};_])@u';
-$agda_lang['keyglyph'] = '@=>|=|->|:|\\\\|<-|\||[∀→_]@u';
-$agda_lang['keyword'] = '@\b(if|then|else|module|open|import|qualified|hiding|where|let|in|case|of|newtype|default|infix|infixr|infixl|data|class|instance|forall|exists|deriving|do|record|field|open|public|private|renaming|postulate|with|__keyword__[[:alnum:]]+)(?=$|[ (){}])@u';
+$agda_lang['keyglyph'] = '@=>|=|->|:|\\\\|<-|\|(?!\|)|[∀→_]@u';
+$agda_lang['keyword'] = '@\b(if|then|else|module|open|import|qualified|hiding|where|let|in|case|of|newtype|default|infix|infixr|infixl|data|class|instance|forall|exists|deriving|do|record|field|open|public|private|renaming|using|as|postulate|with|mutual|syntax|__keyword__[[:alnum:]]+)(?=$|[ (){}])@u';
 $agda_lang['agda-fun']  = "@\b(cong|subst|Set|Eq|Σ|Π|Iso)(?=$|[ (){}])@u";
 $agda_lang['agda-ctor'] = "@\b(refl|,|\[\]|0|zero|suc|here|there|acc)(?=$|[ (){}])@u";
 $agda_lang['agda-proj'] = "@\b(proj₁|proj₂)(?=$|[ (){}])@u";
 $agda_lang['small-space'] = '@(?<=\[)[ ]|[ ](?=\])@u';
 $agda_lang['conop'] = "@[⊎ℕ]@u";
 $agda_lang['varop'] = "@([-:\\@#$%^*.|=+<>&~/\\\\\'⟶≈⟨⟩⌈⌉⌊⌋≤◂≡×≢?∘\\[\\]]|!!?(?!!))+[^ (){}_]*(__[{][^}]*[}])?@u";
-$agda_lang['conid'] = "@\\b(?:[[:upper:]][[:alnum:]']*[.])*[[:upper:]][^ (){}_.;]+(?=$|[ (){};_])@u";
+$agda_lang['conid'] = "@\\b(?:[[:upper:]][[:alnum:]']*[.])*[[:upper:]][^ (){}_.;]+(?=$|[ (){};_])|⊤|⊥@u";
 $agda_lang['varid'] = "@\\b(?:[[:upper:]][[:alnum:]']*[.])*[[:lower:]][^ (){}_.;^]*(?=$|[ (){};_])@u";
 $agda_lang['hole'] = "@[{]!(.*?)![}]@u";
 
